2ed3b35f0f420153c4049cd1eb8f1b6f78f27fb4
MykolaMatsko <mykola.matsko@gmail.com>
2024-04-19 18:17:00 +0300

    1.07-Created

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -16,7 +16,7 @@ namespace MagicVilla_VillaAPI.Controllers
 			return Ok(VillaStore.villaList);
 		}
 
-		[HttpGet("id:int")]
+		[HttpGet("id:int", Name = "GetVilla")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -35,8 +35,15 @@ namespace MagicVilla_VillaAPI.Controllers
 		}
 
 		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		public ActionResult<VillaDTO> CreateVilla ([FromBody] VillaDTO villaDto)
 		{
+			//if (!ModelState.IsValid)
+			//{
+			//	return BadRequest(ModelState);
+			//}
 			if (villaDto ==  null)
 			{
 				return BadRequest(villaDto);
@@ -48,7 +55,7 @@ namespace MagicVilla_VillaAPI.Controllers
 			villaDto.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
 			VillaStore.villaList.Add(villaDto);
 
-			return Ok(villaDto);
+			return CreatedAtRoute("GetVilla", new { id = villaDto.Id}, villaDto);
 		}
 	}
 }
